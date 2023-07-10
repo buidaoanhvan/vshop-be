cf7d589b3b52036a985168d9d983bada3a4a4666
Bùi Đào Anh Văn <buidaoanhvan1997@gmail.com>
2023-07-10 11:12:36 +0700

    build

--- src/app.js
+++ src/app.js
@@ -3,20 +3,20 @@ const cors = require("cors");
 const morgan = require("morgan");
 const helmet = require("helmet");
 const compression = require("compression");
-const promMid = require("express-prometheus-middleware");
+// const promMid = require("express-prometheus-middleware");
 const app = express();
 
 const logger = require("./utils/logger.util");
 
-app.use(
-  promMid({
-    metricsPath: "/metrics",
-    collectDefaultMetrics: true,
-    requestDurationBuckets: [0.1, 0.5, 1, 1.5],
-    requestLengthBuckets: [512, 1024, 5120, 10240, 51200, 102400],
-    responseLengthBuckets: [512, 1024, 5120, 10240, 51200, 102400],
-  })
-);
+// app.use(
+//   promMid({
+//     metricsPath: "/metrics",
+//     collectDefaultMetrics: true,
+//     requestDurationBuckets: [0.1, 0.5, 1, 1.5],
+//     requestLengthBuckets: [512, 1024, 5120, 10240, 51200, 102400],
+//     responseLengthBuckets: [512, 1024, 5120, 10240, 51200, 102400],
+//   })
+// );
 
 const errorHandling = (err, req, res, next) => {
   console.error(err);
